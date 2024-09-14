88e98b365a847600977b6fafcec860d7f11ce6be
Aksh1101 <1101akshat@gmail.com>
2024-09-15 00:07:56 +0530

    Day 3

--- Topics/Standard input with Java Scanner/Adding five to a number, then doubling it/src/Main.kt
+++ Topics/Standard input with Java Scanner/Adding five to a number, then doubling it/src/Main.kt
@@ -2,8 +2,11 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     // TODO: Use the nextInt() method of the Scanner class to read an integer from the standard input.
-
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
     // Then, add five to the read number and multiply the result by two.
-
+    val sum = n + 5
+    val mul = sum * 2
     // Finally, print the result of the multiplication to the standard output.
-}'
+    println(mul)
+}
